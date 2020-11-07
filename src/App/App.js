c494112818827134ba80a8706b6de22bcb2f27be
Naomi Ware <nware1066@yahoo.com>
2020-11-07 16:46:19 -0700

    create function for setting fetched data to state, passed function as prop to MainView

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,28 @@
 import React,  { Component } from 'react';
 import { Route, Link} from 'react-router-dom';
 import MainView from '../MainView/MainView'
+import { getRandomBeer } from '../apiCalls.js';
+import { formatBeers } from '../cleanData.js';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      beer: {}
     }
+  }
+
+  serveBeer = () => {
+    formatBeers()
+    .then(singleBeer => this.setState({beer: singleBeer}))
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <MainView />
+          <MainView serveBeer={this.serveBeer}/>
         </header>
       </div>
     );
