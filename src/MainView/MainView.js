f2b6013b85fe0b4203b8c82a46fab82eb20449c4
Naomi Ware <nware1066@yahoo.com>
2020-11-06 19:20:34 -0700

    Create MainView functional Component

---
+++ src/MainView/MainView.js
@@ -0,0 +1,16 @@
+import React, from 'react';
+import { Link } from 'react-router-dom''
+import './mainView.css';
+
+function MainView(props) {
+  return (
+    <section className='main-view'>
+      <h1>Welcome to the pub</h1>
+      <button className='beer-button'></button>
+      <p>Click the button and get a beer</p>
+    </section>
+  )
+}
+
+export default MainView;
+//
