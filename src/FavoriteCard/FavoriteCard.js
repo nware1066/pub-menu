f44f4468d9c988106232170d76f8c33941027adb
Naomi Ware <nware1066@yahoo.com>
2020-11-08 13:51:55 -0700

    Create folder and files for FavoriteCard

---
+++ src/FavoriteCard/FavoriteCard.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import FoodPairingView from '../FoodPairingView/FoodPairingView';
+
+
+
+export default FavoriteCard;
