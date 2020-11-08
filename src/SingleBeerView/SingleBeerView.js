38db6a75815c4b6fd0a0754b95ef6573da3a18dc
Naomi Ware <nware1066@yahoo.com>
2020-11-07 18:45:51 -0700

    Add food view options to SingleBeerView

--- src/SingleBeerView/SingleBeerView.js
+++ src/SingleBeerView/SingleBeerView.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import FoodPairingView from '../FoodPairingView/FoodPairingView'
 import './singleBeerView.css';
 
 function SingleBeerView(props) {
 
-  return (
-    <section className='single-beer'>
-      <h3>{props.beer.name}</h3>
-      <p>{props.beer.description}</p>
-      <button className='single-beer-button' onClick={props.serveBeer}>I don't want this one</button>
-    </section>
-  )
+  if (props.viewFood) {
+    return (
+      <section className='single-beer'>
+        <p>{props.beer.tagline}</p>
+        <h1>{props.beer.name}</h1>
+        <h2>Goes well with things like:</h2>
+        <FoodPairingView foods={props.beer.foods} viewFood={props.viewFood}/>
+        <button className='add-favorite-button' onClick={() => {props.addToFavorites(props.beer)}}>Add to Favorites</button>
+        <Link to='/favorites'><button className='favorites-button'>Show Me My Favorites</button></Link>
+        <Link to='/'><button className='single-beer-button' onClick={props.showFood}>I Want To See More Beers</button></Link>
+      </section>
+    )
+  } else {
+    return (
+      <section className='single-beer'>
+        <h3>{props.beer.name}</h3>
+        <p>{props.beer.description}</p>
+        <button className='single-beer-button' onClick={props.serveBeer}>I don't want this one</button>
+        <Link to='/food'><button className='single-beer-button' onClick={props.showFood}>Sounds Great! Show me some food that goes with this!</button></Link>
+      </section>
+    )
+  }
 }
 
 export default SingleBeerView;
-//
