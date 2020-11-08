a547e6ffd78356d14e5b875e8a1b9a4ee095cb30
Naomi Ware <nware1066@yahoo.com>
2020-11-08 16:52:04 -0700

    fix syntax for FavoriteCard props

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -3,14 +3,22 @@ import { Link } from 'react-router-dom';
 import FoodPairingView from '../FoodPairingView/FoodPairingView';
 import './favoriteCard.css';
 
-function FavoriteCard(props) {
+// function FavoriteCard(props) {
+//   console.log(props)
+//   return (
+//     <article className='favorite'>
+//       <h1 className='favorite-beer-name'>{props.name}</h1>
+//       <h3>{props.description}</h3>
+//       <button className='favorite-button'>Add to Favorites</button>
+//     </article>
+//   )
+// }
+const FavoriteCard = ({name, description}) => {
   return (
-    <article className='favorite'>
-      <h1>{this.props.beer.name}</h1>
-      <h3>{this.props.beer.discription}</h3>
-      <FoodPairingView />
-      <button className='favorite-button'>Add to Favorites</button>
-    </article>
+    <section>
+      <h1>{name}</h1>
+      <h2>{description}</h2>
+    </section>
   )
 }
 
