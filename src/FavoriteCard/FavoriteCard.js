4687402360ac80135303d75e8cb99d3d32aba47f
Naomi Ware <nware1066@yahoo.com>
2020-11-08 13:55:25 -0700

    Create structure for individual favorite card

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import FoodPairingView from '../FoodPairingView/FoodPairingView';
 
-
+function FavoriteCard(props) {
+  return (
+    <article className='favorite'>
+      <h1>{this.props.beer.name}</h1>
+      <h3>{this.props.beer.discription}</h3>
+      <FoodPairingView />
+      <button className='favorite-button'>Add to Favorites</button>
+    </article>
+  )
+}
 
 export default FavoriteCard;
