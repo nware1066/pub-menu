1b95c7da647925a4ecbe8b1c1ad973e9e3bb1e07
Naomi Ware <nware1066@yahoo.com>
2020-11-08 19:25:40 -0700

    Remove addToFavoritesButton from FavoriteCard and add classNames for styling

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -3,22 +3,13 @@ import { Link } from 'react-router-dom';
 import FoodPairingView from '../FoodPairingView/FoodPairingView';
 import './favoriteCard.css';
 
-// function FavoriteCard(props) {
-//   console.log(props)
-//   return (
-//     <article className='favorite'>
-//       <h1 className='favorite-beer-name'>{props.name}</h1>
-//       <h3>{props.description}</h3>
-//       <button className='favorite-button'>Add to Favorites</button>
-//     </article>
-//   )
-// }
 const FavoriteCard = ({name, description}) => {
+
   return (
-    <section>
-      <h1>{name}</h1>
-      <h2>{description}</h2>
-    </section>
+    <section className='favorite-card'>
+        <h1 className='favorite-name'>{name}</h1>
+        <h2 className='favorite-description'>{description}</h2>
+      </section>
   )
 }
 
