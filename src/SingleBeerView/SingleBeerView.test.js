3bab852fbc543e93b36b5d587acac99dca0c7ef0
Naomi Ware <nware1066@yahoo.com>
2020-11-09 11:11:22 -0700

    Create unit test for food button and showFood function

--- src/SingleBeerView/SingleBeerView.test.js
+++ src/SingleBeerView/SingleBeerView.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import SingleBeerView from './SingleBeerView';
 
 describe('SingleBeerView', () => {
-  it('should display beer information', () => {
-    let mockServeBeer = jest.fn()
+  it('should display beer information', async () => {
     const mockBeer = [
       {
         "id": 1,
@@ -15,18 +14,28 @@ describe('SingleBeerView', () => {
         "description": "beer1 is a great beer"
       }
     ]
+    let mockServeBeer = jest.fn()
+    let mockShowFood = jest.fn()
 
     render(
       <MemoryRouter>
         <SingleBeerView
           beer={mockBeer}
           serveBeer={mockServeBeer}
+          showFood={mockShowFood}
         />
       </MemoryRouter>
     )
+
+    const beerButton = screen.getByText("I don't want this one")
+    const foodButton = screen.getByRole('button', { name: /sounds great! show me some food that goes with this!/i })
+
     expect(screen.getByText("I don't want this one")).toBeInTheDocument()
-    userEvent.click(screen.getByText("I don't want this one"))
+    fireEvent.click(beerButton)
+    screen.debug()
     expect(mockServeBeer).toHaveBeenCalledTimes(1)
+    fireEvent.click(foodButton)
+    expect(mockShowFood).toHaveBeenCalledTimes(1)
   })
 })
 //
