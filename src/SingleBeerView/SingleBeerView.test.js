e36d9ceea9a393f40c5aa23aad2b167916122b54
Naomi Ware <nware1066@yahoo.com>
2020-11-08 21:46:52 -0700

    Set up test file and create initial test for SingleBeerView

---
+++ src/SingleBeerView/SingleBeerView.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import SingleBeerView from './SingleBeerView';
+
+describe('SingleBeerView', () => {
+  it('should display beer information', () => {
+    let mockServeBeer = jest.fn()
+    const mockBeer = [
+      {
+        "id": 1,
+        "name": "beer1",
+        "tagline": "beer1 tagline",
+        "description": "beer1 is a great beer"
+      }
+    ]
+
+    render(
+      <MemoryRouter>
+        <SingleBeerView
+          beer={mockBeer}
+          serveBeer={mockServeBeer}
+        />
+      </MemoryRouter>
+    )
+    expect(screen.getByText("I don't want this one")).toBeInTheDocument()
+  })
+})
+//
