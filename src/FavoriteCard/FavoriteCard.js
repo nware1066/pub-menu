39c2585dc6c2fa5d48a99ddc7fc6d8b1fb793f66
Naomi Ware <nware1066@yahoo.com>
2020-11-09 16:37:13 -0700

    Add FoodPairingView to FavoriteCard

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import FoodPairingView from '../FoodPairingView/FoodPairingView';
 import './favoriteCard.css';
 
-const FavoriteCard = ({name, description}) => {
+const FavoriteCard = ({name, description, foods}) => {
 
   return (
     <section className='favorite-card'>
         <h1 className='favorite-name'>{name}</h1>
         <h2 className='favorite-description'>{description}</h2>
+        <FoodPairingView foods={foods}/>
       </section>
   )
 }
