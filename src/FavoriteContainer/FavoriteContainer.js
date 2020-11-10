d01d6bdb4bbbd18b7f17d06e47a53497daa38880
Naomi Ware <nware1066@yahoo.com>
2020-11-09 19:01:04 -0700

    Add PropTypes and key to FavoriteContainer

--- src/FavoriteContainer/FavoriteContainer.js
+++ src/FavoriteContainer/FavoriteContainer.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FavoriteCard from '../FavoriteCard/FavoriteCard';
-import FoodPairingView from '../FoodPairingView/FoodPairingView'
 import './favoriteContainer.css';
 
 const FavoriteContainer = ({favorites}) => {
-  console.log(favorites)
   return (
       <section className='favorite-container'>
-        <>
           { favorites.map(favorite => {
               return <FavoriteCard
+              key={`${favorite.id}-${favorite.name}`}
               name={favorite.name}
               foods={favorite.foods}
               description={favorite.description} />
             })
           }
-        </>
       </section>
   )
 }
 export default FavoriteContainer;
+
+FavoriteContainer.propTypes = {
+  favorite: PropTypes.object
+}
